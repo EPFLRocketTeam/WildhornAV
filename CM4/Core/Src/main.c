30fd27e832eaf9d9f4f341930a172e7c3a980d16
ofacklam <oliver.facklam@gmail.com>
2022-02-16 21:52:40 +0100

    Improved u-boot & kernel device trees (not working yet)

--- CM4/Core/Src/main.c
+++ CM4/Core/Src/main.c
@@ -707,10 +707,6 @@ static void MX_TIM3_Init(void)
   sConfigOC.Pulse = 0;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
   if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
--- CM4/Core/Src/stm32mp1xx_hal_msp.c
+++ CM4/Core/Src/stm32mp1xx_hal_msp.c
@@ -749,11 +749,10 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     __HAL_RCC_GPIOB_CLK_ENABLE();
     /**TIM3 GPIO Configuration
     PC7     ------> TIM3_CH2
-    PC6     ------> TIM3_CH1
     PB1     ------> TIM3_CH4
     PB0     ------> TIM3_CH3
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_6;
+    GPIO_InitStruct.Pin = GPIO_PIN_7;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
