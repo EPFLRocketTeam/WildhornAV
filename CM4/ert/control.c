15006557198bf52ddda4dbf2962a87492f3a1563
iacopo <iacopo@pop-os.localdomain>
2022-07-14 20:57:21 +0200

    remote proc virtual uart working

--- CM4/ert/control.c
+++ CM4/ert/control.c
@@ -159,15 +159,17 @@ void control_thread(__attribute__((unused)) void * arg) {
 	device_interface_t * hostproc_interface = hostproc_get_feedback_interface();
 
 
-
 	for(;;) {
 
 
 		static uint8_t data[32];
 		static uint32_t len = 32;
+		len = 32;
 		device_interface_recv(hostproc_interface, data, &len);
-		static uint8_t msg[] = "hello\n";
-		device_interface_send(hostproc_interface, msg, sizeof(msg));
+		if(len > 0) {
+			device_interface_send(hostproc_interface, data, len);
+		}
+
 
 
 		vTaskDelayUntil( &last_wake_time, period );
